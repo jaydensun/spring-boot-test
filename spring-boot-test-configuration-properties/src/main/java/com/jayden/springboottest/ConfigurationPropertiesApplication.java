9875e006ca2768dfe65df5e71074818974293c56
089245 <sunyongjun@sf-express.com>
2019-07-19 14:08:17 +0800

    use spring boot bom

--- spring-boot-test-configuration-properties/src/main/java/com/jayden/springboottest/ConfigurationPropertiesApplication.java
+++ spring-boot-test-configuration-properties/src/main/java/com/jayden/springboottest/ConfigurationPropertiesApplication.java
@@ -2,12 +2,10 @@ package com.jayden.springboottest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-@ImportResource("classpath:spring-config.xml")
-public class Application {
+public class ConfigurationPropertiesApplication {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class);
+        SpringApplication.run(ConfigurationPropertiesApplication.class);
     }
 }
--- spring-boot-test-disconf/src/main/java/com/jayden/springboottest/DisconfApplication.java
+++ spring-boot-test-disconf/src/main/java/com/jayden/springboottest/DisconfApplication.java
@@ -2,10 +2,12 @@ package com.jayden.springboottest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-public class Application {
+@ImportResource("classpath:spring-config.xml")
+public class DisconfApplication {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class);
+        SpringApplication.run(DisconfApplication.class);
     }
 }
