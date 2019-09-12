771853c4cc7c7e2563946e1c1dede4fbf9aeda3c
089245 <sunyongjun@sf-express.com>
2019-09-12 14:45:25 +0800

    exclude PropertyPlaceholderAutoConfiguration

--- spring-boot-test-disconf/src/main/java/com/jayden/springboottest/DisconfApplication.java
+++ spring-boot-test-disconf/src/main/java/com/jayden/springboottest/DisconfApplication.java
@@ -2,9 +2,10 @@ package com.jayden.springboottest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.context.PropertyPlaceholderAutoConfiguration;
 import org.springframework.context.annotation.ImportResource;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = PropertyPlaceholderAutoConfiguration.class)
 @ImportResource("classpath:spring-config.xml")
 public class DisconfApplication {
     public static void main(String[] args) {
