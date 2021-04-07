92e158d39aacedb67045fec185f6a6b33f5c2647
tandysun <tandysun@tencent.com>
2021-04-07 18:13:17 +0800

    add encrypt db password test

---
+++ spring-boot-test-db-encrypt/src/main/java/com/jayden/springbootestdb/DbEncryptApp.java
@@ -0,0 +1,15 @@
+package com.jayden.springbootestdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 孙勇军（089245）
+ * @since 2019/12/4
+ */
+@SpringBootApplication
+public class DbEncryptApp {
+    public static void main(String[] args) {
+        SpringApplication.run(DbEncryptApp.class);
+    }
+}
---
+++ spring-boot-test-db-encrypt/src/main/java/com/jayden/springbootestdb/PasswordEncrypt.java
@@ -0,0 +1,9 @@
+package com.jayden.springbootestdb;
+
+import com.alibaba.druid.filter.config.ConfigTools;
+
+public class PasswordEncrypt {
+    public static void main(String[] args) throws Exception {
+        ConfigTools.main(new String[]{"zhikong@123"});
+    }
+}
